8f5d4cd3e05701494f590a5257594c563f4bbb60
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-08-19 12:32:27 +0200

    feat: Default stats test passed

--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -1,14 +1,18 @@
 package com.factoriaf5.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
     
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
-    }
+    public void whenCharacterIsCreatedDefaultStats() {
+        Character character = new Character();
 
+        assertEquals(1000, character.getHealth());
+        assertEquals(1, character.getLevel());
+        assertTrue(character.isAlive());
+    }
 }
