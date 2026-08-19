014c1bc24a853a6434f59bbc0a2389f865bf8a4b
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-08-19 11:06:09 +0200

    feat: Stats
    Character's health, level and alive created

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -2,4 +2,13 @@ package com.factoriaf5.kata;
 
 public class Character {
     
+    private int health;
+    private int level;
+    private boolean alive;
+
+    public Character() {
+        this.health = 1000;
+        this.level = 1;
+        this.alive = true;
+    }
 }
